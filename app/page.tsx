ca6bae8d9758b45b7b08bc006b16e3e6a39454cf
Luka la d <lukas-8@live.fr>
2024-05-16 14:34:08 +0200

    fix: disconnect button

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { useAccount, useDisconnect } from "wagmi";
 import ConnectButton from "./components/connectButton";
 
 export default function Home() {
   const account = useAccount();
   const { disconnect } = useDisconnect();
 
+  console.log(account.status);
+
   return (
     <main className="flex min-h-screen flex-col items-center p-12">
-      {account.status == "connected" ? <button onClick={() => {disconnect}}>Disconnect</button> : <ConnectButton />}
+      {account.status == "connected" ? <button onClick={() => {disconnect()}}>Disconnect</button> : <ConnectButton />}
       Paste your transaction in here.
     </main>
   );
