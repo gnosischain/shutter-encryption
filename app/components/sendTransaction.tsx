278f1413a4f6d34137a51bddfed3fa5384cf9c08
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2024-06-17 18:05:02 +0200

    include encryption script

--- app/components/sendTransaction.tsx
+++ app/components/sendTransaction.tsx
@@ -1,17 +1,21 @@
-import { computeData } from "@/actions/encryptTxNobleCurves";
+// import { computeData } from "@/actions/encryptTxNobleCurves";
 import { Input } from "@headlessui/react";
 import clsx from "clsx";
-import * as React from "react";
 import { Address, parseEther } from "viem";
 import { config } from "@/wagmi";
-import { type BaseError, useWaitForTransactionReceipt, useWriteContract } from "wagmi";
+import {
+  type BaseError,
+  useWaitForTransactionReceipt,
+  useWriteContract,
+} from "wagmi";
 import { getPublicClient } from "wagmi/actions";
 import keyperSetManagerABI from "@/utils/abis/keyperSetManager";
 import keyBroadcastABI from "@/utils/abis/keyBroadcast";
 import sequencerABI from "@/utils/abis/sequencer";
 import { randomBytes } from "crypto";
-import { prepareAndSignTransaction } from "@/actions/createRawTx";
-import { encryptData } from "@/actions/encryptTxNobleCurvesFullBlst";
+import { runTests } from "@/shutter/testEncryption";
+// import { prepareAndSignTransaction } from "@/actions/createRawTx";
+// import { encryptData } from "@/actions/encryptTxNobleCurvesFullBlst";
 
 const SEQUENCER = "0xd073BD5A717Dce1832890f2Fdd9F4fBC4555e41A";
 const KEYPERSETMANAGER = "0x7Fbc29C682f59f809583bFEE0fc50F1e4eb77774";
@@ -23,8 +27,16 @@ interface SendTransactionProps {
   chainId: number | undefined;
 }
 
+declare global {
+  interface Window {
+    blst: any;
+  }
+}
+
 export function SendTransaction({ address, chainId }: SendTransactionProps) {
-  const client = getPublicClient(config, { chainId: chainId as 100 | 10200 | undefined });
+  const client = getPublicClient(config, {
+    chainId: chainId as 100 | 10200 | undefined,
+  });
   const { data: hash, error, isPending, writeContract } = useWriteContract();
   // const data = prepareAndSignTransaction();
 
@@ -50,25 +62,65 @@ export function SendTransaction({ address, chainId }: SendTransactionProps) {
       const randomBytesBuffer = randomBytes(12);
       const randomHex = randomBytesBuffer.toString("hex");
       const identityPrefixHex = address + randomHex;
-      const encryptedTx = await encryptData(data, identityPrefixHex as `0x{string}`, eonKeyBytes, identityPrefixHex as `0x{string}`);
-      writeContract({ address: SEQUENCER, abi: sequencerABI, functionName: "submitEncryptedTransaction", args: [eon, identityPrefixHex, encryptedTx, 210000], value: parseEther("210000", "gwei") });
+      // const encryptedTx = await encryptData(
+      //   data,
+      //   identityPrefixHex as `0x{string}`,
+      //   eonKeyBytes,
+      //   identityPrefixHex as `0x{string}`
+      // );
+      // writeContract({
+      //   address: SEQUENCER,
+      //   abi: sequencerABI,
+      //   functionName: "submitEncryptedTransaction",
+      //   args: [eon, identityPrefixHex, encryptedTx, 210000],
+      //   value: parseEther("210000", "gwei"),
+      // });
     }
   }
 
-  const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({
-    hash,
-  });
+  const { isLoading: isConfirming, isSuccess: isConfirmed } =
+    useWaitForTransactionReceipt({
+      hash,
+    });
+
+  const testEncryption = async () => {
+    if (!window.blst) {
+      console.log("blst not loaded");
+      return;
+    }
+    runTests();
+  };
 
   return (
     <form onSubmit={submit}>
-      <Input name="data" type="text" required placeholder="0xA0Cf…251e" className={clsx("mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white", "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25")} />
+      <Input
+        name="data"
+        type="text"
+        required
+        placeholder="0xA0Cf…251e"
+        className={clsx(
+          "mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white",
+          "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
+        )}
+      />
       <button disabled={isPending} type="submit">
         {isPending ? "Confirming..." : "Send"}
       </button>
       {hash && <div>Transaction Hash: {hash}</div>}
       {isConfirming && <div>Waiting for confirmation...</div>}
       {isConfirmed && <div>Transaction confirmed.</div>}
-      {error && <div>Error: {(error as BaseError).shortMessage || error.message}</div>}
+      {error && (
+        <div>Error: {(error as BaseError).shortMessage || error.message}</div>
+      )}
+      <div>
+        <button
+          className="rounded-md bg-black/20 border border-white text-white py-2 px-4 text-sm m-4"
+          onClick={testEncryption}
+          type="button"
+        >
+          Test encryption
+        </button>
+      </div>
     </form>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Script from "next/script";
 import { useAccount, useDisconnect } from "wagmi";
 import ConnectButton from "./components/connectButton";
 import { SendTransaction } from "./components/sendTransaction";
@@ -9,22 +10,28 @@ export default function Home() {
   const { disconnect } = useDisconnect();
 
   return (
-    <main className="flex h-screen bg-stone-900 flex-col items-center p-12">
-      {account.status == "connected" ? (
-        <button
-          onClick={() => {
-            disconnect();
-          }}
-        >
-          Disconnect
-        </button>
-      ) : (
-        <ConnectButton />
-      )}
-      <div className="flex flex-col items-center mt-4 text-nowrap">
-        <p className="mr-2">Paste your transaction in here:</p>
-        <SendTransaction address={account.address} chainId={account.chainId} />
-      </div>
-    </main>
+    <>
+      <Script src="/blst/blst.js" />
+      <main className="flex h-screen bg-stone-900 flex-col items-center p-12">
+        {account.status == "connected" ? (
+          <button
+            onClick={() => {
+              disconnect();
+            }}
+          >
+            Disconnect
+          </button>
+        ) : (
+          <ConnectButton />
+        )}
+        <div className="flex flex-col items-center mt-4 text-nowrap">
+          <p className="mr-2">Paste your transaction in here:</p>
+          <SendTransaction
+            address={account.address}
+            chainId={account.chainId}
+          />
+        </div>
+      </main>
+    </>
   );
 }
