10ade91b7ef4e7400aa01d6bfa68c63585130dc2
verkhohliad <ihor.verkhohliad@gmail.com>
2024-07-31 15:08:26 +0100

    Initialize subgraph

---
+++ thegraph/m-shutter-validator-registry/src/validator-registry.ts
@@ -0,0 +1,16 @@
+import { Updated as UpdatedEvent } from "../generated/ValidatorRegistry/ValidatorRegistry"
+import { Updated } from "../generated/schema"
+
+export function handleUpdated(event: UpdatedEvent): void {
+  let entity = new Updated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.message = event.params.message
+  entity.signature = event.params.signature
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
