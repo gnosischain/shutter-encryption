cc33b8f179a0a5cd2374214b2e328e5816338fa4
verkhohliad <ihor.verkhohliad@gmail.com>
2024-09-02 16:54:41 +0100

    fix: fix Web3ModalProvider, remove extra exports

--- src/providers/Web3ModalProvider.tsx
+++ src/providers/Web3ModalProvider.tsx
@@ -4,7 +4,6 @@ import { gnosis, gnosisChiado } from 'wagmi/chains';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import config from '@/constants/config';
-import { Web3Modal } from '@web3modal/wagmi';
 
 const BRAWE_WALLET_ID = '163d2cf19babf05eb8962e9748f9ebe613ed52ebf9c8107c9a0f104bfcf161b3';
 
@@ -13,7 +12,7 @@ const queryClient = new QueryClient();
 const projectId = config.walletConnectProjectId;
 
 const chains = [gnosisChiado, gnosis] as const;
-export const wagmiConfig = createConfig({
+const wagmiConfig = createConfig({
   chains,
   transports: {
     [gnosis.id]: http(),
@@ -21,7 +20,7 @@ export const wagmiConfig = createConfig({
   },
 });
 
-export const modal: Web3Modal = createWeb3Modal({
+createWeb3Modal({
   wagmiConfig,
   projectId,
   featuredWalletIds: [BRAWE_WALLET_ID],
